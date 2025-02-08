25dfd0bd6118e753c119f2999da5a23d5a0b3ddc
cankurtur-global <cankurtur+global@gmail.com>
2025-02-08 19:40:22 +0100

    create base error

---
+++ GlobalNetworking/Base/Errors.swift
@@ -0,0 +1,79 @@
+//
+//  Errors.swift
+//  GlobalNetworking
+//
+//  Created by Can Kurtur on 8.02.2025.
+//
+
+import Foundation
+
+/// Generic error type for networking layer. You can define your own error type by conforming to this protocol.
+public protocol APIError: Codable, AnyObject {
+    var error: String { get }
+    var statusCode: Int? { get set }
+}
+
+/// Defines various error types that can occur in APIClient.
+public enum APIClientError: Error {
+    case handledError(error: APIError)
+    case networkError
+    case decodingError(error: DecodingError?)
+    case timeout
+    case invalidStatusCode
+    case badRequest
+    
+    /// Human readable error message.
+    public var message: String {
+        switch self {
+        case .handledError(let error):
+            return error.error
+        case .networkError:
+            return "Could not connect to the server"
+        case .decodingError:
+            return "Failed to decode response"
+        case .timeout:
+            return "Request timed out"
+        case .invalidStatusCode:
+            return "Invalid response from server"
+        case .badRequest:
+            return "Bad request"
+        }
+    }
+    
+    /// Debug message, providing more detailed error information.
+    public var debugMessage: String {
+        switch self {
+        case .handledError(let error):
+            return error.error
+        case .networkError:
+            return "Network error"
+        case .decodingError(let error):
+            guard let decodingError = error else { return "Decoding Error" }
+            return "Decoding Error: \(decodingError)"
+        case .timeout:
+            return "Timeout"
+        case .invalidStatusCode:
+            return "Invalid status code"
+        case .badRequest:
+            return "Bad request"
+        }
+    }
+    
+    /// Corresponding HTTP status code.
+    public var statusCode: Int {
+        switch self {
+        case .handledError(let error):
+            return error.statusCode ?? 0
+        case .networkError:
+            return NSURLErrorNotConnectedToInternet
+        case .decodingError:
+            return NSURLErrorCannotDecodeRawData
+        case .timeout:
+            return NSURLErrorTimedOut
+        case .invalidStatusCode:
+            return 0
+        case .badRequest:
+            return 400
+        }
+    }
+}
