41382ce1d570fadd845c8e7f005c8338fe18550c
cankurtur-global <cankurtur+global@gmail.com>
2025-02-09 10:06:02 +0100

    Develop to Main (#16)
    * change Xcode compatibility version to 14.0
    
    * Bugfix/revert compatibility version (#7)
    
    * Revert "change Xcode compatibility version to 14.0 (#6)"
    
    This reverts commit 28091b7c581a119f3ca482f5b8bb9e3f03dd2a3d.
    
    * delete old xcframework
    
    * add new xcframework
    
    * Refactor/comment refactor (#11)
    
    * refactor comment
    
    * delete old xcframework
    
    * add new xcframework
    
    * Add Default Logger (#13)
    
    * create default network logger
    
    * add logger as a parameter
    
    * delete xcframework
    
    * re-generate xcframework
    
    * Remove associatedtype to limit endpointitem (#15)
    
    * remove EndpointItem associatedtype from protocol
    
    * delete xcframework
    
    * re-generate xcframework

--- GlobalNetworking/Base/NetworkManager/NetworkManagerProtocol.swift
+++ GlobalNetworking/Base/NetworkManager/NetworkManagerProtocol.swift
@@ -10,8 +10,7 @@ import Combine
 
 /// A protocol defining the requirements for a network manager that handles API requests using Combine, async/await, and callback-based approaches.
 public protocol NetworkManagerProtocol {
-    associatedtype EndpointItem: Endpoint
-    func request<T: Decodable>(endpoint: EndpointItem, responseType: T.Type) -> AnyPublisher<T, APIClientError>
-    func request<T: Decodable>(endpoint: EndpointItem, responseType: T.Type) async throws -> T
-    func request<T: Decodable>(endpoint: EndpointItem, responseType: T.Type, completion: @escaping NetworkHandler<T>)
+    func request<T: Decodable>(endpoint: some Endpoint, responseType: T.Type) -> AnyPublisher<T, APIClientError>
+    func request<T: Decodable>(endpoint: some Endpoint, responseType: T.Type) async throws -> T
+    func request<T: Decodable>(endpoint: some Endpoint, responseType: T.Type, completion: @escaping NetworkHandler<T>)
 }
