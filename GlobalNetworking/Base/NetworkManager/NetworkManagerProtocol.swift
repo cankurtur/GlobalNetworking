33ed8c831c38b0b76ea9f58998fd433519b95474
cankurtur-global <cankurtur+global@gmail.com>
2025-02-10 20:24:00 +0100

    Protocol refactoring (#21)
    * add associatedtype to make endpoints limited
    
    * remove old xcframework
    
    * add xcframework

--- GlobalNetworking/Base/NetworkManager/NetworkManagerProtocol.swift
+++ GlobalNetworking/Base/NetworkManager/NetworkManagerProtocol.swift
@@ -10,7 +10,8 @@ import Combine
 
 /// A protocol defining the requirements for a network manager that handles API requests using Combine, async/await, and callback-based approaches.
 public protocol NetworkManagerProtocol {
-    func request<T: Decodable>(endpoint: some Endpoint, responseType: T.Type) -> AnyPublisher<T, APIClientError>
-    func request<T: Decodable>(endpoint: some Endpoint, responseType: T.Type) async throws -> T
-    func request<T: Decodable>(endpoint: some Endpoint, responseType: T.Type, completion: @escaping NetworkHandler<T>)
+    associatedtype EndpointItem: Endpoint
+    func request<T: Decodable>(endpoint: EndpointItem, responseType: T.Type) -> AnyPublisher<T, APIClientError>
+    func request<T: Decodable>(endpoint: EndpointItem, responseType: T.Type) async throws -> T
+    func request<T: Decodable>(endpoint: EndpointItem, responseType: T.Type, completion: @escaping NetworkHandler<T>)
 }
